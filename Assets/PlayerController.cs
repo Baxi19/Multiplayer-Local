f2ed3f65386594ce5e545d635449224185d7c1d7
Baxi19 <randald1991@gmail.com>
2021-05-11 19:03:40 -0600

    WASD With new Input System :fire:

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float spped = 5;
+    private Vector2 movementInput;
+    private void Update() {
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * spped * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+}
